b10f9f96e4909c79aed38db643c5979307db0ad4
uditt.lamba@gmail.com <uditt.lamba@gmail.com>
2020-10-18 17:44:33 +0530

    bugfix

--- replicant-backend/tests/replicant.spec.js
+++ replicant-backend/tests/replicant.spec.js
@@ -14,27 +14,27 @@ describe('checkAccountUpdate', () => {
         .resolves;
   });
 });
-// describe('checkSubredditPopulate', () => {
-//   it('should call subredditPopulateHandler and return', async () => {
-//     expect(await subredditPopulateHandler())
-//         .resolves;
-//   });
-// });
+describe('checkSubredditPopulate', () => {
+  it('should call subredditPopulateHandler and return', async () => {
+    expect(await subredditPopulateHandler())
+        .resolves;
+  });
+});
 describe('checkTopPostFetch', () => {
   it('should call topPostFetchHandler and return', () => {
     expect(topPostFetchHandler())
         .resolves;
   });
 });
-// describe('checkPostSchedule', () => {
-//   it('should call postScheduleHandler and return', async () => {
-//     expect(await postScheduleHandler())
-//         .resolves;
-//   });
-// });
-// describe('checkKarmaFarming', () => {
-//   it('should call karmaFarmingHandler and return', async () => {
-//     expect( await karmaFarmingHandler())
-//         .resolves;
-//   });
-// });
+describe('checkPostSchedule', () => {
+  it('should call postScheduleHandler and return', async () => {
+    expect(await postScheduleHandler())
+        .resolves;
+  });
+});
+describe('checkKarmaFarming', () => {
+  it('should call karmaFarmingHandler and return', async () => {
+    expect( await karmaFarmingHandler())
+        .resolves;
+  });
+});
