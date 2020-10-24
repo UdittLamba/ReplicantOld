04e7f5699dff3a1581c8c185c918b02efa16a30a
uditt.lamba@gmail.com <uditt.lamba@gmail.com>
2020-10-24 15:08:49 +0530

    test js file tweak

--- replicant-backend/tests/replicant.spec.js
+++ replicant-backend/tests/replicant.spec.js
@@ -2,12 +2,13 @@ import {test} from '@jest/globals';
 
 const {describe, it} = require('@jest/globals');
 
-const {accountUpdateHandler,
+const {
+  accountUpdateHandler,
   subredditPopulateHandler,
   topPostFetchHandler,
   postScheduleHandler,
-  karmaFarmingHandler} = require('../handler');
-
+  karmaFarmingHandler,
+} = require('../handler');
 
 // describe('checkAccountUpdate', () => {
 //   it('should call accountUpdateHandler and resolve', async () => {
@@ -17,7 +18,7 @@ const {accountUpdateHandler,
 // });
 describe('checkSubredditPopulate', () => {
   it('should call subredditPopulateHandler and resolve', async () => {
-    try{
+    try {
       await subredditPopulateHandler();
     } catch (e) {
       expect(e).toMatch('error');
@@ -25,35 +26,35 @@ describe('checkSubredditPopulate', () => {
   });
 });
 describe('checkTopPostFetch', () => {
-test('should call topPostFetchHandler and resolve', async () => {
+  test('should call topPostFetchHandler and resolve', async () => {
     expect.assertions(1);
-    try{
+    try {
       await topPostFetchHandler();
-    }catch (e) {
-      expect(e).toMatch('error');
-    }
-});
-  });
-
-  describe('checkPostSchedule', () => {
-  test('should call postScheduleHandler and resolve', async () => {
-    expect.assertions(1);
-    try{
-      await postScheduleHandler()
     } catch (e) {
       expect(e).toMatch('error');
     }
   });
-  });
+});
 
-    describe('checkSubredditPopulate', () => {
+describe('checkPostSchedule', () => {
+  test('should call postScheduleHandler and resolve', async () => {
+    expect.assertions(1);
+    try {
+      await postScheduleHandler();
+    } catch (e) {
+      expect(e).toMatch('error');
+    }
+  });
+});
+
+describe('checkSubredditPopulate', () => {
   test('should call karmaFarmingHandler and resolve', async () => {
     expect.assertions(1);
-    try{
+    try {
       await subredditPopulateHandler();
     } catch (e) {
       expect(e).toMatch('error');
     }
 
   });
-    });
+});
