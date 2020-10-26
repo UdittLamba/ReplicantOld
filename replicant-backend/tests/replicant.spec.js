bc1138ba1a203e8927e5c1e734184fd79ff6e635
uditt.lamba@gmail.com <uditt.lamba@gmail.com>
2020-10-26 16:30:50 +0530

    some tweaks to replicant.spec.js

--- replicant-backend/tests/replicant.spec.js
+++ replicant-backend/tests/replicant.spec.js
@@ -14,7 +14,7 @@ describe('checkAccountUpdate', () => {
     } catch (e) {
       expect(e).toMatch('error');
     }
-  }, 6000);
+  }, 10000);
 });
 describe('checkSubredditPopulate', () => {
   test('should call subredditPopulateHandler and resolve', async () => {
@@ -23,7 +23,7 @@ describe('checkSubredditPopulate', () => {
     } catch (e) {
       expect(e).toMatch('error');
     }
-  }, 6000);
+  });
 });
 describe('checkTopPostFetch', () => {
   test('should call topPostFetchHandler and resolve', async () => {
@@ -32,7 +32,7 @@ describe('checkTopPostFetch', () => {
     } catch (e) {
       expect(e).toMatch('error');
     }
-  }, 6000);
+  }, 10000);
 });
 
 describe('checkPostSchedule', () => {
@@ -42,7 +42,7 @@ describe('checkPostSchedule', () => {
     } catch (e) {
       expect(e).toMatch('error');
     }
-  }, 6000);
+  });
 });
 
 describe('checkKarmaFarming', () => {
